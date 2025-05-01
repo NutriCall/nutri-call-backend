7f985c387e9477e4b46f4df89065710f772d04bc
Vangzbill <mamadhan017@gmail.com>
2025-05-01 18:08:39 +0700

    fix: Update NGROK_URL to the latest endpoint

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:@127.0.0.1:3307/nutri_app")
 APP_URL = os.getenv("APP_URL", "http://localhost:8878/api")
-NGROK_URL = "https://ce25-180-248-19-155.ngrok-free.app/api"
+NGROK_URL = "https://1237-180-248-19-155.ngrok-free.app/api"
 SECRET_KEY = os.getenv("SECRET_KEY") 
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 432000 
