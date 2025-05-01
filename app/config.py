44b648f364fb742c7eb4b0fe56d0243dbc9e15c7
Vangzbill <mamadhan017@gmail.com>
2025-05-01 20:52:06 +0700

    fix: Update database and application URLs in config.py

--- app/config.py
+++ app/config.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:@127.0.0.1:3307/nutri_app")
-APP_URL = os.getenv("APP_URL", "http://localhost:8878/api")
-NGROK_URL = "https://1237-180-248-19-155.ngrok-free.app/api"
+DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://dev_ta:SabilDidil00?@8.215.11.94:3306/nutri_app")
+APP_URL = os.getenv("APP_URL", "http://178.128.30.50:8000/api")
+NGROK_URL = os.getenv("NGROK_URL", "http://178.128.30.50:8000/api")
 SECRET_KEY = os.getenv("SECRET_KEY") 
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 432000 
