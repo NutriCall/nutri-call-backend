229f2d9595022ba6f5e4a310545ef3a18b8bf231
Vangzbill <mamadhan017@gmail.com>
2025-05-02 02:34:39 +0700

    fix: Update APP_URL and NGROK_URL to use the secure endpoint

--- app/config.py
+++ app/config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-APP_URL = os.getenv("APP_URL", "http://178.128.30.50:8000/api")
-NGROK_URL = os.getenv("NGROK_URL", "http://178.128.30.50:8000/api")
+APP_URL = os.getenv("APP_URL", "https://byte-together.tech/api")
+NGROK_URL = os.getenv("NGROK_URL", "https://byte-together.tech/api")
 SECRET_KEY = os.getenv("SECRET_KEY") 
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 432000 
