56d4b5bf345c48a097f90ef34409bcd63b037b7e
Vangzbill <mamadhan017@gmail.com>
2025-05-01 21:19:14 +0700

    fix: Remove default DATABASE_URL in config.py to enforce environment variable usage

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://dev_ta:SabilDidil00?@8.215.11.94:3306/nutri_app")
+DATABASE_URL = os.getenv("DATABASE_URL")
 APP_URL = os.getenv("APP_URL", "http://178.128.30.50:8000/api")
 NGROK_URL = os.getenv("NGROK_URL", "http://178.128.30.50:8000/api")
 SECRET_KEY = os.getenv("SECRET_KEY") 
